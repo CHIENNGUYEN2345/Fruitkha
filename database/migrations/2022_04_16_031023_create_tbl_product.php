22fc6b0e6b8886afe3b920f87ca0cdb4d85ba3ca
NPHC <huuchien.np03@gmail.com>
2022-06-09 01:07:26 +0700

    change 3

--- database/migrations/2022_04_16_031023_create_tbl_product.php
+++ database/migrations/2022_04_16_031023_create_tbl_product.php
@@ -14,7 +14,7 @@ class CreateTblProduct extends Migration
     public function up()
     {
         Schema::create('tbl_product', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('product_id');
             $table->integer('category_id');
             $table->integer('brand_id');
             $table->text('product_desc');
